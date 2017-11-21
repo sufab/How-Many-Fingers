eb6f1133161f97767cb87668f75bd83925af3985
sufa bitton <sufabitton@sufas-imac>
2017-11-21 13:08:28 +0200

    Initial Commit

---
+++ How many fingers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  How many fingers
+//
+//  Created by sufa bitton on 21/11/2017.
+//  Copyright © 2017 sufa bitton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
